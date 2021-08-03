271538382edd1a677d6ec8be83caa6fa7eda7739
THONWELLING <thonwellingdani13@gmail.com>
2021-08-03 01:08:01 -0300

    iniciado sidebar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Container } from './style';
+import React, { useState } from 'react';
+import { Container, Search } from './style';
+import  TextField, { Input }  from '@material/react-text-field';
+import logo from '../../assets/logo.svg';
+import { useState } from 'react';
+
+
 
 const Home = () => {
+    const [inputValue, setInputValue] =useState('');
     return (
     <Container>
-        Hello World!
+        <Search>
+            <img src={logo} alt="logo do restaurante" />
+            < TextField 
+                label = 'Pesquisar' 
+                outLined
+                onTrailingIconSelect = {() => this.setState({value:''})} 
+                //trailingIcon = {<PapeldeMaterialIcon ="botão"icon="delete" />} 
+                > < Input 
+                value = { this . estado . valor } 
+                onChange= {(e) => setInputValue(e.target.value)} /> 
+            </TextField>
+        </Search>
     </Container>
     );
 }
