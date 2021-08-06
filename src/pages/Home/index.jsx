8e7b66b3f8bf1b0a5bfd96d11444dcd4554dc65b
THONWELLING <thonwellingdani13@gmail.com>
2021-08-06 17:32:18 -0300

    implementação do mapa início

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import Slider from "react-slick";
-import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './style';
+import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle } from './style';
 import  TextField, { Input }  from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 
 import restaurante from '../../assets/restaurante-fake.png';
 import logo from '../../assets/logo.svg';
-import { Card, RestaurantCard, Modal } from '../../components';
+import { Card, RestaurantCard, Modal, Map } from '../../components';
 
 
 
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
-    const [modalOpened, setModalOpened] = useState(false);
+    const [modalOpened, setModalOpened] = useState(true);
     const settings = {
         dots: false,
         infinite: true,
@@ -54,12 +54,11 @@ const Home = () => {
                     <Card photo={restaurante} title="unknow name" />
                     <Card photo={restaurante} title="unknow name" />
                 </Carousel>
-                <button onClick={() => setModalOpened(true)}>Abrir Modal</button>
             </Search>
             <RestaurantCard />
         </Container>
         <Map />
-        <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
+        {/* <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} /> */}
     </Wrapper>
     );
 };
